c6db125c2ed18fc000f22bee5a49420cb49c769a
dao-84 <cattafidao@gmail.com>
2026-04-01 00:10:15 +0200

    fix(watermark): aumenta opacità logo dal 40% al 75%

--- src/utils/imageHelpers.js
+++ src/utils/imageHelpers.js
@@ -148,7 +148,7 @@ async function applyWatermark(buffer) {
   const logoWithAlpha = await sharp(logoResized)
     .composite([{
       input: {
-        create: { width: lw, height: lh, channels: 4, background: { r: 0, g: 0, b: 0, alpha: 0.40 } }
+        create: { width: lw, height: lh, channels: 4, background: { r: 0, g: 0, b: 0, alpha: 0.75 } }
       },
       blend: 'dest-in',
     }])
